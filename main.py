99399a609c27699ca5d16f0bd81112e7792d4248
Lukas Benedix <lbenedix@l3q.de>
2022-04-25 22:28:24 +0200

    refactor and save stuff to db

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from typing import Tuple
 import minify_html
 
 import requests
-from feedparser import FeedParserDict
+from feedgen.entry import FeedEntry
 from readability import Document
 from bs4 import BeautifulSoup as bs, Tag
 import feedparser
 from feedgen.feed import FeedGenerator
+from tinydb import TinyDB, Query
 
-url = "http://www.lvz.de/rss/feed/lvz_region"
-source_feed = feedparser.parse(url)
+feeds = [
+    "http://www.lvz.de/rss/feed/lvz_leipzig",
+    "http://www.lvz.de/rss/feed/lvz_region",
+    "http://www.lvz.de/rss/feed/lvz_kultur",
+]
 
 
 def get_tag_html(tag: Tag):
@@ -24,8 +28,8 @@ def get_summary(url: str) -> Tuple[str, str]:
 
     try:
         soup = bs(response.text, "html.parser")
-        plus_open = "pdb-parts-paidcontent-freeuntilbadge_close"
-        plus_close = "pdb-parts-paidcontent-freeuntilbadge_open"
+        plus_close = "pdb-parts-paidcontent-freeuntilbadge_close"
+        plus_open = "pdb-parts-paidcontent-freeuntilbadge_open"
 
         prefix = "free"
         if len(soup.findAll("span", {"class": plus_open})) > 0:
@@ -42,62 +46,80 @@ def get_summary(url: str) -> Tuple[str, str]:
     except:
         return prefix, ""
 
-# <item>
-#     <title>Unbekannte schmieren in Roßwein gegen die Polizei</title>
-#     <link>http://www.lvz.de/Region/Unbekannte-schmieren-in-Rosswein-gegen-die-Polizei</link>
-#     <description>&lt;p class="pda-textparagraph"&gt;Unbekannte Täter verunstalteten die Fassade eines ehemaligen
-#         Schulkomplexes in Roßwein. Der Sachschaden beträgt etwa 10 000 Euro. Warum es sich dabei auch um eine Attacke
-#         gegen die Polizei handelt.&lt;/p&gt;&lt;p class="pda-textparagraph"&gt;&lt;!-- Methode uuid:
-#         "f292ad36-c484-11ec-825e-f5cc19365745"--&gt;&lt;/p&gt;
-#     </description>
-#     <author>mol-lvz@madsack.de (LVZ Methode)</author>
-#     <guid isPermaLink="false">f292ad36-c484-11ec-825e-f5cc19365745</guid>
-#     <pubDate>Mon, 25 Apr 2022 11:07:33 +0000</pubDate>
-# </item>
 
-# {'title': 'Unbekannte schmieren in Roßwein gegen die Polizei',
-#  'title_detail': {'type': 'text/plain', 'language': 'de-DE', 'base': 'https://www.lvz.de/rss/feed/lvz_region',
-#                   'value': 'Unbekannte schmieren in Roßwein gegen die Polizei'}, 'links': [
-#     {'rel': 'alternate', 'type': 'text/html',
-#      'href': 'http://www.lvz.de/Region/Unbekannte-schmieren-in-Rosswein-gegen-die-Polizei'}],
-#  'link': 'http://www.lvz.de/Region/Unbekannte-schmieren-in-Rosswein-gegen-die-Polizei',
-#  'summary': '<p class="pda-textparagraph">Unbekannte Täter verunstalteten die Fassade eines ehemaligen Schulkomplexes in Roßwein. Der Sachschaden beträgt etwa 10 000 Euro. Warum es sich dabei auch um eine Attacke gegen die Polizei handelt.</p><p class="pda-textparagraph"><!-- Methode uuid: "f292ad36-c484-11ec-825e-f5cc19365745"--></p>',
-#  'summary_detail': {'type': 'text/html', 'language': 'de-DE', 'base': 'https://www.lvz.de/rss/feed/lvz_region',
-#                     'value': '<p class="pda-textparagraph">Unbekannte Täter verunstalteten die Fassade eines ehemaligen Schulkomplexes in Roßwein. Der Sachschaden beträgt etwa 10 000 Euro. Warum es sich dabei auch um eine Attacke gegen die Polizei handelt.</p><p class="pda-textparagraph"><!-- Methode uuid: "f292ad36-c484-11ec-825e-f5cc19365745"--></p>'},
-#  'authors': [{'name': 'LVZ Methode', 'email': 'mol-lvz@madsack.de'}], 'author': 'mol-lvz@madsack.de (LVZ Methode)',
-#  'author_detail': {'name': 'LVZ Methode', 'email': 'mol-lvz@madsack.de'}, 'id': 'f292ad36-c484-11ec-825e-f5cc19365745',
-#  'guidislink': False, 'published': 'Mon, 25 Apr 2022 11:07:33 +0000',
-#  'published_parsed': time.struct_time(tm_year=2022, tm_mon=4, tm_mday=25, tm_hour=11, tm_min=7, tm_sec=33, tm_wday=0,
-#                                       tm_yday=115, tm_isdst=0)}
+def setup_feed(source_feed, url) -> FeedGenerator:
+    fg = FeedGenerator()
+    fg.id(url)
+    fg.title(source_feed.feed.title)
+    fg.author(source_feed.feed.authors[0])
+    fg.link(href=source_feed.feed.link, rel='alternate')
+    fg.logo('https://www.lvz.de/bundles/molasset/images/sites/desktop/lvz/apple-touch-icon.png')
+    fg.subtitle(source_feed.feed.subtitle)
+    fg.link(href=url, rel='self')
+    fg.language(source_feed.feed.language)
 
-from feedgen.feed import FeedGenerator
+    return fg
 
-fg = FeedGenerator()
-fg.id(url)
-fg.title(source_feed.feed.title)
-fg.author(source_feed.feed.authors[0])
-fg.link(href=source_feed.feed.link, rel='alternate')
-fg.logo('https://www.lvz.de/bundles/molasset/images/sites/desktop/lvz/apple-touch-icon.png')
-fg.subtitle(source_feed.feed.subtitle)
-fg.link(href=url, rel='self')
-fg.language(source_feed.feed.language)
 
-done = set()
-for entry in source_feed.entries:
-    if entry.guid in done:
-        continue
+def to_fe(item: dict) -> FeedEntry:
+    fe = FeedEntry()
+    fe.id(item['guid'])
+    fe.title(item['title'])
+    fe.link(href=item['link'])
+    fe.summary(item['summary'])
+    fe.author(item['author'])
+    fe.published(item['published'])
+    return fe
 
-    is_premium, summary = get_summary(entry.link)
-    title = entry.title if not is_premium else f'💸 - {entry.title}'
 
-    fe = fg.add_entry()
-    fe.id(entry.guid)
-    fe.title(title)
-    fe.link(href=entry.link)
-    fe.summary(summary)
-    fe.author(entry.authors[0])
+def get_items(url: str):
+    print('📖 getting all items', url)
+    feed_id = url.split('/')[-1]
+    db = TinyDB(f'{feed_id}.json')
 
-    done.add(entry.guid)
-    print(fe.id())
+    source_feed = feedparser.parse(url)
 
-fg.atom_file('atom.xml')
+    for entry in source_feed.entries:
+        Item = Query()
+        if len(db.search(Item.guid == entry.guid)) > 0:
+            print('skip')
+            continue
+
+        prefix, summary = get_summary(entry.link)
+
+        if prefix == '+ closed':
+            print('skip paywall')
+            continue
+
+        title = f'{prefix} - {entry.title}'
+
+        item = {
+            "guid": entry.guid,
+            "title": title,
+            "link": entry.link,
+            "summary": summary,
+            "author": entry.authors[0],
+            "published": entry.published
+        }
+        db.insert(item)
+
+
+def generate_feed(url: str):
+    print('📝 generating feed', url)
+    feed_id = url.split('/')[-1]
+    db = TinyDB(f'{feed_id}.json')
+    source_feed = feedparser.parse(url)
+
+    fg = setup_feed(source_feed, url)
+    for item in db.all():
+        fg.add_entry(to_fe(item))
+
+    fg.atom_file(f"{feed_id}_atom.xml")
+
+
+if __name__ == '__main__':
+
+    for feed_url in feeds:
+        print(feed_url)
+        get_items(feed_url)
+        generate_feed(feed_url)
